ff8f33de356c9c252a0f1884010ef5539963e3e2
Selim <selim@technovicinity.com>
2025-02-04 11:58:14 +0600

    New fetures added

--- src/app/modules/post/post.interface.ts
+++ src/app/modules/post/post.interface.ts
@@ -1,9 +1,7 @@
-import { Date, Types } from "mongoose";
+import { Types } from "mongoose";
 
 export type TPost = {
   title: string;
   content: string;
   authorId: Types.ObjectId;
-  createdAt: Date;
-  updatedAt: Date;
 };
