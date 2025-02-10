6b5a5a1ff31f641fd4c4a38185ad80ff1121b61b
Selim <selim@technovicinity.com>
2025-02-10 14:51:58 +0600

    add error handlers in global error handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,6 +1,11 @@
 import { ErrorRequestHandler } from "express";
 
+import { ZodError } from "zod";
 import AppError from "../error/AppError";
+import handleCastError from "../error/handleCastError";
+import handleDuplicateError from "../error/handleDuplicateError";
+import handleValidationError from "../error/handleValidationError";
+import handleZodError from "../error/handleZodError";
 import { TErrorSources } from "../interface/error";
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
@@ -14,7 +19,28 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     },
   ];
 
-  if (error instanceof AppError) {
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  } else if (error.name === "ValidationError") {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error.name == "CastError") {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error.code == "11000") {
+    const simplifiedError = handleDuplicateError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (error instanceof AppError) {
     statusCode = error?.statusCode;
     message = error?.message;
     errorSources = [{ path: "", message: error?.message }];
