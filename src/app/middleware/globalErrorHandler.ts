8c00c4ef8ef9fb1105f92f28612b9de3415e48fd
Selim <selim@technovicinity.com>
2025-02-10 10:42:52 +0600

    message bug fixed in Error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -19,7 +19,7 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     message = error?.message;
     errorSources = [{ path: "", message: error?.message }];
   } else if (error instanceof Error) {
-    message = error?.message;
+    message = error.issues[0].message;
     errorSources = [{ path: "", message: error?.message }];
   }
 
