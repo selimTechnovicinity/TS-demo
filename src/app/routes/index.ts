2bb2983c947bc17ed77153dfff2d9ac2c1422a34
Selim <selim@technovicinity.com>
2025-02-10 10:41:52 +0600

    product route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { PaypalRoutes } from "../modules/paypal/paypal.route";
 import { PostRoutes } from "../modules/post/post.route";
+import { ProductRoutes } from "../modules/product/product.route";
 import { UserRoutes } from "../modules/user/user.route";
 
 const router = Router();
@@ -10,6 +11,10 @@ const moduleRoutes = [
   {
     path: "/users",
     route: UserRoutes,
+  },
+  {
+    path: "/products",
+    route: ProductRoutes,
   },
   {
     path: "/auth",
